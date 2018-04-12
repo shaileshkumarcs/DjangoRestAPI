cf296ba53356802e5ef41d954fe748b286f1653d
shailesh <shailesh.kumarcs@gmail.com>
2018-04-12 23:11:40 +0530

    Lession 12 comleted and start on custom permission

--- posts/api/serializers.py
+++ posts/api/serializers.py
@@ -2,15 +2,17 @@ from rest_framework.serializers import ModelSerializer
 
 from posts.models import Post
 
-class PostListSerializer(ModelSerializer):
+class PostCreateUpdateSerializer(ModelSerializer):
     class Meta:
         model = Post
         fields = [
+            #'id',
             'title',
-            'slug',
+            #'slug',
             'content',
             'publish',
         ]
+
 
 class PostDetailsSerializer(ModelSerializer):
     class Meta:
@@ -24,15 +26,32 @@ class PostDetailsSerializer(ModelSerializer):
         ]
 
 
+class PostListSerializer(ModelSerializer):
+    class Meta:
+        model = Post
+        fields = [
+            'user',
+            'title',
+            'slug',
+            'content',
+            'publish',
+        ]
+
+
+
+
 """"
+from posts.models import Post
+from posts.api.serializers import PostDetailsSerializer
+
 data = {
     "title": "Yeahh buddy",
     "content": "New Content",
     "publish": "2016-2-12",
     "slug": "Yeahh-Buddy",
 }
-
-new_item = PostSerializer(data=data)
+obj = Post.objects.get(id=2)
+new_item = PostDetailsSerializer(obj, data=data)
 if new_item.is_valid():
     new_item.save()
 else:
--- posts/api/views.py
+++ posts/api/views.py
@@ -1,9 +1,54 @@
-from rest_framework.generics import ListAPIView, RetrieveAPIView
+from rest_framework.generics import (
+    CreateAPIView,
+    DestroyAPIView,
+    ListAPIView,
+    RetrieveAPIView,
+    UpdateAPIView,
+    RetrieveUpdateAPIView
+    )
+
+from rest_framework.permissions import (
+    AllowAny,
+    IsAuthenticated,
+    IsAdminUser,
+    IsAuthenticatedOrReadOnly,
+    )
 
 from posts.models import Post
-from .serializers import PostListSerializer, PostDetailsSerializer
+
+from .permissions import IsOwnerOrReadOnly
+
+from .serializers import (
+    PostCreateUpdateSerializer,
+    PostListSerializer,
+    PostDetailsSerializer
+    )
+
+class PostCreateAPIView(CreateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostCreateUpdateSerializer
+    permission_classes = [IsAuthenticated]
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
 
 class PostDetailsAPIView(RetrieveAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostDetailsSerializer
+    lookup_field = "slug"
+   # lookup_url_kwarg = "abc"
+
+class PostUpdateAPIView(RetrieveUpdateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostCreateUpdateSerializer
+    lookup_field = "slug"
+   # lookup_url_kwarg = "abc"
+    permission_classes = [IsAuthenticatedOrReadOnly,IsOwnerOrReadOnly]
+
+    def perform_update(self, serializer):
+        serializer.save(user=self.request.user)
+
+class PostDeleteAPIView(DestroyAPIView):
     queryset = Post.objects.all()
     serializer_class = PostDetailsSerializer
     lookup_field = "slug"
